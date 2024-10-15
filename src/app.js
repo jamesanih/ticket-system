1f84d8e3a1a1f574186edb2b32e625537ecae0c2
james <uchennaanih16@gmail.com>
2024-10-15 15:14:48 +0100

    added a basic logger using winston

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const eventRoutes = require('./routes/eventRoutes');
 const userRoutes = require('./routes/userRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const swagger = require('./utils/swagger');
+const logger = require('./utils/logger');
 
 const app = express();
 
@@ -12,7 +13,26 @@ app.use('/api-docs', swagger.serve, swagger.setup);
 app.use('/api/users', userRoutes);
 app.use('/api/events', eventRoutes);
 
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request for ${req.url}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  logger.info('Handling request for root route');
+  res.send('Hello, World!');
+});
+
+app.use((err, req, res, next) => {
+  logger.error('An error occurred', { error: err.message, stack: err.stack });
+  res.status(500).send('Something went wrong');
+});
 
 app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
